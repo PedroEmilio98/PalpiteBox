f0eca21ba3ed63d0e32abb5f23932e5046d52be7
Pedro Emilio <pedroemiliokuhn@gmail.com>
2021-09-07 00:21:32 -0300

    base64

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,5 +1,5 @@
 import { GoogleSpreadsheet, } from 'google-spreadsheet';
-
+import { fromBase64 } from '../../utils/base64';
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_ID)
 
@@ -11,7 +11,7 @@ export default async (req, res) => {
         //await doc.useServiceAccountAuth(credentials);
         doc.useServiceAccountAuth({
             client_email: process.env.SHEET_CLIENT_EMAIL,
-            private_key: process.env.SHEET_PRIVATE_KEY
+            private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
         })
         await doc.loadInfo();
 
